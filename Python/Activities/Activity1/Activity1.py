267c27b5a10bc5135e410acb0b2599cfde605a2a
Pranayreddy159 <116861636+Pranayreddy159@users.noreply.github.com>
2022-11-01 17:02:00 +0530

    Add files via upload

---
+++ Python/Activities/Activity1/Activity1.py
@@ -0,0 +1,4 @@
+name=input("Enter your name:")
+age=int(input("Enter you age:"))
+estyear=2022+100-age
+print(name+",You will turn age of 100 in the year "+str(estyear))
---
+++ Python/Activities/Activity2/Activity2.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number:"))
+div=num%2
+if div==0:
+    print(str(num)+" is a even number")
+else:
+    print(str(num)+" is a odd number")
